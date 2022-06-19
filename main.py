0f550d1417d66acb4a3726d092633745fbe3b0c1
Silboi <silvan.roffler@stud.fhgr.ch>
2022-06-19 22:02:10 +0200

    Commit

--- main.py
+++ main.py
@@ -10,53 +10,41 @@ import json
 app = Flask(__name__)
 
 
-#@app.route("/asdf")
-#def home():
- #   about_link = url_for("home")
-  #  return render_template("index.html", link=about_link)
-
-
-@app.route('/', methods=["get", "post"]) #formular erstellt bzw. verknüpfung, mit get und post wird entgegengenommen und wiedergegeben
+@app.route('/', methods=["get", "post"])            #formular erstellt bzw. verknüpfung, mit get und post wird entgegengenommen und wiedergegeben
 def formular():
     if request.method.lower() == "post":
         zutaten = request.form["erste_zutat"]
-        return redirect(url_for("zutaten"))
+        return redirect(url_for("zutaten"))         #zu dieser Seite leiten
     else:
         return render_template("formular.html")
 
 
 @app.route("/zutaten", methods=["get", "post"])
 def zutaten():
-    zutaten_1 = ""
-  #  zutaten_2 = ""
-   # zutaten_3 = ""
-    drinks_match = []
-    karte = {}
-    gesamtpreis = 0
+    zutaten_1 = ""                          #Variable erstellt, zuerst leer
+    gesamtpreis = 0                         #Preis definiert, zuerst auf 0
 
     if request.method.lower() == "post":
-        zutaten_1 = request.form["erste_zutat".lower()]
-   #     zutaten_2 = request.form["zweite_zutat"]
-    #    zutaten_3 = request.form["dritte_zutat"]
+        zutaten_1 = request.form["erste_zutat"]
 
-    karte = dictionary_aus_json()
-    drinks_match = []
+    karte = dictionary_aus_json()           #Daten von json in Variable gespeichert
+    drinks_match = []                       #Leerer Dictionary, um diesen später zu füllen
 
-    for key, value in karte.items():
-        for key2, value2 in value.items():
-            for key3, value3 in value2.items():
-                if key3 == zutaten_1:
-                    drinks_match.append([key, 0])
+    for key, value in karte.items():                #Loop, um auf die nächste Ebene zu gelangen im Dictionary
+        for key2, value2 in value.items():          #Loop, um auf die nächste Ebene zu gelangen im Dictionary, neuer key und value name
+            for key3, value3 in value2.items():     #Loop, um auf die nächste Ebene zu gelangen im Dictionary, neuer key und value name
+                if key3 == zutaten_1:               #Abfrage, ob eine Zutat übereinstimmt mit den Daten
+                    drinks_match.append([key, 0])   #Drinkname (oberste Ebene im Dictionary) in Liste laden für die Ausgabe
 
-    for key, value in karte.items():
-        for element in drinks_match:
-            if key == element[0]:
-                for key2, value2 in value.items():
-                    for key3, value3 in value2.items():
-                        gesamtpreis = gesamtpreis + value3["Preis"]
-                        element[1] = gesamtpreis
+    for key, value in karte.items():                                    #Loop, um auf die nächste Ebene zu gelangen im Dictionary
+        for element in drinks_match:                                    #Loop für Abfrage
+            if key == element[0]:                                       #Vergleich ob Drinkname übereinstimmt mit Abfrage
+                for key2, value2 in value.items():                      #Loop, um auf die nächste Ebene zu gelangen im Dictionary, neuer key und value name
+                    for key3, value3 in value2.items():                 #Loop, um auf die nächste Ebene zu gelangen im Dictionary, neuer key und value name
+                        gesamtpreis = gesamtpreis + value3["Preis"]     #Preis definieren und dazurechnen
+                        element[1] = gesamtpreis                        #Speicherung
 
-    gesamtpreis = 0
+    gesamtpreis = 0                             #wieder auf 0 setzen
 
     if len(drinks_match) < 1:                                                   #wenn die Eingabe ungültig ist, dann gibt es das aus
         drinks_match.append(["Diese Zutat ist in keinem Drink vorhanden", ""])          #mit dem Platzhalter, weil es 2 braucht und es unsichtbar ist, wenn es leer ist
@@ -64,15 +52,15 @@ def zutaten():
     return render_template("zutaten.html", zutaten1=zutaten_1, drinks_match=drinks_match, karte=karte)
 
 
-def dictionary_aus_json():          # Daten von der json Datei werden geladen
+def dictionary_aus_json():              # Daten von der json Datei werden geladen
     with open("data.json") as file:
         data = json.load(file)
     return data
 
 
-@app.route("/liste")        # Daten werden ausgegeben
+@app.route("/liste")             # Daten werden ausgegeben
 def auflisten():
-    drinks = dictionary_aus_json()
+    drinks = dictionary_aus_json()              #Speicherung von den Daten
 
     return render_template("liste.html", drinks=drinks)
 
